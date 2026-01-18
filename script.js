98cd1b4e517d755a04d07d824e76d8a33328483c
mr-moon313 <mallichandru007@gmail.com>
2026-01-18 12:43:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -96,48 +96,6 @@ window.addEventListener("load", () => {
     container.addEventListener("mouseenter", () => clearTimeout(autoplayTimer));
     container.addEventListener("mouseleave", autoplay);
 
-    <script>
-const canvas = document.getElementById("stars");
-const ctx = canvas.getContext("2d");
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-let stars = [];
-
-for (let i = 0; i < 200; i++) {
-  stars.push({
-    x: Math.random() * canvas.width,
-    y: Math.random() * canvas.height,
-    r: Math.random() * 1.5,
-    dx: Math.random() * 0.3,
-    dy: Math.random() * 0.3
-  });
-}
-
-function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "white";
-
-  stars.forEach(s => {
-    ctx.beginPath();
-    ctx.arc(s.x, s.y, s.r, 0, Math.PI * 2);
-    ctx.fill();
-
-    s.x += s.dx;
-    s.y += s.dy;
-
-    if (s.x > canvas.width) s.x = 0;
-    if (s.y > canvas.height) s.y = 0;
-  });
-
-  requestAnimationFrame(animate);
-}
-
-animate();
-</script>
-
-
     autoplay();
   });
 });
